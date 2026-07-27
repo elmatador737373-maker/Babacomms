750918a660f042ba6a1407fdf6d97c4b905b60ea
elmatador737373-maker <elmatador737373@gmail.com>
2026-07-27 22:21:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask
 from dotenv import load_dotenv
 import discord
 from discord import app_commands
+from discord.ext import commands  # <--- Questa riga mancava o era cancellata!
 import requests
 from supabase import create_client, Client
 
